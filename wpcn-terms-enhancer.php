6983e8256a9d7b0b50bea455667dd80cd39826db
WPConstructor <198608876+WPConstructor@users.noreply.github.com>
2026-05-13 17:01:56 +0200

    Add composer.json with autoloader, move classes to src (PSR-4), add .gitattributes and .gitignore, move constants.php to src/

--- wpcn-terms-enhancer.php
+++ wpcn-terms-enhancer.php
@@ -34,59 +34,13 @@ if ( ! defined( 'WPINC' ) ) {
 	die();
 }
 
+// Require Composer autoloader.
+require_once __DIR__ . '/vendor/autoload.php';
+
 // Sets plugin dirs of the parent dir.
 define( __NAMESPACE__ . '\\MAIN_FILE', __FILE__ );
 
 // Requires and sets the constants used in this plugin.
-require_once __DIR__ . '/inc/constants.php';
-
-/**
- * Registers a custom class autoloader function.
- *
- * @param callable $autoload_function The autoload function to register.
- * @param bool $throw                 Whether to throw exceptions if the autoload function cannot load the requested class.
- *                                    Defaults to true.
- * @param bool $prepend               Whether to prepend the autoloader to the stack instead of appending it.
- *                                    Defaults to false, meaning the autoloader will be appended.
- *
- * @version 1.0.0
- * @since 1.0.0
- *
- * @return bool Returns true on success or false on failure.
- */
-spl_autoload_register(
-	/**
-	 * Auto loads class.
-	 *
-	 * @version 1.0.0
-	 * @since 1.0.0
-	 *
-	 * @param string $required_class The class name.
-	 * @return bool Returns true on success or false on failure.
-	 */
-	function ( $required_class ) {
-		$required_class = strtolower( str_replace( '\\', '/', $required_class ) );
-		$pieces         = explode( '/', $required_class );
-		if ( strtolower( 'WPConstructor' ) === $pieces[0] && strtolower( 'TermsEnhancer' ) === $pieces[1] ) {
-			unset( $pieces[0] );
-			unset( $pieces[1] );
-			$pieces = array_values( $pieces );
-		} else {
-			return false;
-		}
-		$pieces[ count( $pieces ) - 1 ] = 'class-' . str_replace( '_', '-', $pieces[ count( $pieces ) - 1 ] ) . '.php';
-
-		$relative_path = implode( '/', $pieces );
-		$path          = plugin_dir_path( __FILE__ ) . 'classes/' . $relative_path;
-
-		if ( file_exists( $path ) ) {
-			require_once $path;
-			return true;
-		}
-		return false;
-	},
-	true,
-	true
-);
+require_once __DIR__ . '/src/constants.php';
 
 new Bootstrap();
